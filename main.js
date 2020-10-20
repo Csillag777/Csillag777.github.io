c2e399d96253e2685789d824244574878b5e7a23
Csillag <sailorchien@gmail.com>
2020-10-20 23:33:15 +0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var checklist = document.getElementsByClassName("checklist");
+=======
+/*var checklist = document.getElementsByClassName("checklist");
+>>>>>>> 13b1dda (test)
 var items = document.querySelectorAll("li");
 var inputs = document.querySelectorAll("input");
 for (var i = 0;i<items.length;i++){
@@ -14,6 +18,7 @@ function write(){
 function update(){
     this.previousElementSibling.innerHTML = this.value;
     this.parentNode.className = "";
+<<<<<<< HEAD
 }
 document.getElementById("11").onclick = function() {alert("goodjob")};
 function flask_open(){
@@ -22,3 +27,39 @@ function flask_open(){
 function w(){
     window.location.href = "f.html";
 }
+=======
+}*/
+//document.getElementById("11").onclick = function() {alert("goodjob")};
+var i = "intro.html";
+function homeredirect(){
+    window.location.href = "index.html";
+}
+function pageredirect(){
+    window.location.href = i;
+}
+function introredirect(){
+    window.location.href = "intro.html";
+}
+function eduredirect(){
+    window.location.href = "edu.html";
+}
+
+function proredirect(){
+    window.location.href = "pro.html";
+}
+
+function conredirect(){
+    window.location.href = "contact.html";
+}
+
+function supredirect(){
+    window.location.href = "sup.html";
+}
+
+function resredirect(){
+    window.location.href = "res.html";
+}
+function apologize(){
+    alert("Sorry, This language is still preparing. TAT");
+}
+>>>>>>> 13b1dda (test)
